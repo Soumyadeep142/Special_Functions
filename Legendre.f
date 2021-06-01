4cf086ddd7927f61deac700200a5540869360985
Soumyadeep142 <55705076+Soumyadeep142@users.noreply.github.com>
2021-06-02 02:18:33 +0530

    Update Legendre.f

--- Legendre.f
+++ Legendre.f
@@ -5,17 +5,15 @@
       x=-1
       dx=0.01
       do 1 j=1,200
-      Po=0
-      Pi=1
-      do 39 n=1,m
-	      Pii=((2*n+1)*x*Pi-n*Po)/(n+1)
-
-	      Po=Pi
-	      Pi=Pii
-	  
+	      Po=0
+	      Pi=1
+	      do 39 n=1,m
+		      Pii=((2*n+1)*x*Pi-n*Po)/(n+1)
+		      Po=Pi
+		      Pi=Pii
  39   continue
-      write(2,*)x, Pi
-      x=x+dx
+	      write(2,*)x, Pi
+	      x=x+dx
  1    continue
       stop
       end
